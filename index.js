9ad1b5521a34699c0ad6628c75ff2ab876ce3688
Camilo <Camilo@MacBook-Air-de-Camilo.local>
2020-10-08 23:09:25 -0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 process.env.PORT = process.env.PORT || 8080;
-const { PORT } = process.env;
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname)));
-app.listen(PORT, () => console.log(`Server is running on port ${ PORT }`));
+app.use(express.static(__dirname));
+app.listen(process.env.PORT, error => console.log(error || `Server is running`));
