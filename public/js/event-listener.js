49ec4f50482ac8775741bacdaee445c27c008a5b
Camilo <Camilo@MacBook-Air-de-Camilo.local>
2020-10-11 16:25:28 -0300

    Added css and js folder outside assets folder

---
+++ public/js/event-listener.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //SELECTORS
+    const collapsible = document.querySelectorAll('.collapsible');
+    const carousel = document.querySelectorAll('.carousel');
+    const form = document.querySelector("form");
+    const role = document.querySelectorAll('.role');
+    const name = document.querySelector('#name');
+    const establishment = document.querySelector('#establishment');
+    const email = document.querySelector('#email');
+    const phone = document.querySelector('#phone');
+    const comment = document.querySelector('#comment');
+    const bottomPage = document.querySelector('#bottom-page');
+
+    //RESET VALUES
+    role.forEach(e => e.checked = e.value === 'Directivo');
+    name.value = '';
+    establishment.value = '';
+    email.value = '';
+    phone.value = '';
+    comment.value = '';
+    bottomPage.disabled = true;
+    bottomPage.classList.add('disabled-btn');
+
+    //INITIALIZE COLLAPSIBLE AND CAROUSEL
+    M.Collapsible.init(collapsible, { accordion: false });
+    M.Carousel.init(carousel, { indicators: true, fullWidth: true });
+
+    //EVENTS
+    form.onsubmit = sendForm;
+    form.oninput = () => {
+        const wrongInput = !!form.querySelector('.wrong-input');
+        const emptyInput = !!Object.values(form.querySelectorAll('input, textarea')).find(e => !e.value);
+        const disabledBtn = wrongInput || emptyInput;
+        bottomPage.disabled = disabledBtn;
+        disabledBtn ? bottomPage.classList.add('disabled-btn') : bottomPage.classList.remove('disabled-btn');
+    }
+
+    form.onsubmit = sendForm;
+
+    const validateField = (event, regex) => {
+        const { target } = event;
+        const { minLength, value } = target;
+        const { length } = value.trim();
+
+        if (!length) {
+            target.classList.add('wrong-input');
+            return target.value = '';
+        };
+
+        if (regex) target.value = value.replace(regex, '');
+
+        const minimalLength = !(target.value.length < minLength);
+
+        !minimalLength ? target.classList.add('wrong-input') : target.classList.remove('wrong-input');
+
+        return minimalLength;
+    }
+
+    const trimField = event => event.target.value = event.target.value.trim();
+
+    name.oninput = event => validateField(event, /[^a-zA-Z ]/);
+    name.onblur = trimField;
+
+    establishment.oninput = event => validateField(event, /[^a-zA-Z0-9-. ]/);
+    establishment.onblur = trimField;
+
+    email.oninput = validateField;
+
+    phone.oninput = event => validateField(event, /[^0-9]/);
+
+    comment.oninput = validateField;
+    comment.onblur = trimField;
+});
