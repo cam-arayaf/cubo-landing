d2411f30fa6180fcde451dad44cb3409cb75ab55
Camilo <Camilo@MacBook-Air-de-Camilo.local>
2020-10-11 10:36:02 -0300

    Created public folder

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(`${ __dirname }/public`));
 app.listen(process.env.PORT, error => console.log(error || 'Server is running'));
